dc88d10437717f83e4dc4f708d6cc1cca0fefa1d
luciannomas <luciano.mastran@gmail.com>
2024-03-15 12:16:36 -0300

    added logout and profile data

---
+++ src/app/api/auth/logout/route.js
@@ -0,0 +1,33 @@
+import { NextResponse } from "next/server";
+import { cookies } from "next/headers";
+
+export function GET() {
+  const cookieStore = cookies();
+  const token = cookieStore.get("myTokenName");
+
+  if (!token) {
+    return NextResponse.json({
+      message: "Not logged in",
+    }, {
+      status: 401,
+    })
+  }
+
+  try {
+    cookieStore.delete("myTokenName");
+
+    const response = NextResponse.json(
+      {},
+      {
+        status: 200,
+      }
+    );
+
+    return response;
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json(error.message, {
+      status: 500,
+    });
+  }
+}
--- src/app/dashboard/profile/page.js
+++ src/app/dashboard/profile/page.js
@@ -7,6 +7,8 @@ import { AccountProfileDetails } from '../../ui/sections/account/account-profile
 import 'simplebar-react/dist/simplebar.min.css';
 import { ThemeProvider } from '@mui/material/styles';
 import { createTheme } from '../../ui/theme/cooming';
+import axios, { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
 
 const style = {
   position: 'absolute',
@@ -21,6 +23,24 @@ const style = {
 };
 function Categories() {
   const theme = createTheme();
+  const router = useRouter();
+
+  const logout = async () => {
+  
+    try {
+      const res = await axios.get("/api/auth/logout");
+      console.log(res);
+    } catch (error) {
+      console.error(error.message);
+    }
+    router.push("/login");
+  };
+
+  const getProfile = async () => {
+    const profile = await axios.get("/api/profile");
+    console.log("profile", profile)
+  };
+
   return (
     <DashLayout>
       <ThemeProvider theme={theme}>
@@ -57,6 +77,8 @@ function Categories() {
                     lg={8}
                   >
                     <AccountProfileDetails />
+                    <button onClick={() => getProfile()}>profile</button>
+                    <button onClick={() => logout()}>Logout</button>
                   </Grid>
                 </Grid>
               </div>
