700d38dd5732e2d8c8a01a2d0a444edae9504f9e
luciannomas <luciano.mastran@gmail.com>
2024-03-15 18:00:36 -0300

    testing profile

--- src/app/api/auth/profile/route.js
+++ src/app/api/auth/profile/route.js
@@ -1,26 +1,35 @@
 import { NextResponse } from "next/server";
-import jwt from "jsonwebtoken";
 import { cookies } from "next/headers";
-import mongoose from "mongoose";
 
-export async function GET() {
+export function GET() {
     try {
-        console.log("ok")
+        const cookieStore = cookies();
+        const token = cookieStore.get("myTokenName");
+        console.log("entro!!")
+
+        if (!token) {
+            return NextResponse.json({ error: "Not logged in" }, { status: 401 });
+        }
+
+        /* // console.log("very",jwt.verify(token.value, "TOKEN_SECRET"))
+        const { email, id } = jwt.verify(token.value, "TOKEN_SECRET");
+
         return NextResponse.json({
-            message: 'ok'
-        })
+            email,
+            id,
+        }); */
+
+        const response = NextResponse.json(
+            {},
+            {
+              status: 200,
+            }
+          );
 
     } catch (error) {
-        if (error instanceof mongoose.Error.ValidationError) {
-            return NextResponse.json(
-                {
-                    message: error.message,
-                },
-                {
-                    status: 400,
-                }
-            );
-        }
-        return NextResponse.error();
+        console.log(error);
+        return NextResponse.json(error.message, {
+            status: 500,
+        });
     }
 }
